436584df581f73b25a8b2c7d44db5fd8a509ffbd
Fernando <fernando.csconceicao@outlook.com>
2022-10-27 19:41:44 -0300

    Inicio dos tempos

---
+++ src/main/java/com/lemomcrab/TribunaldoZodiacoBackend/TribunaldoZodiacoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lemomcrab.TribunaldoZodiacoBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TribunaldoZodiacoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TribunaldoZodiacoBackendApplication.class, args);
+	}
+
+}
